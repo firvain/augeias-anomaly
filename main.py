035b9c8f34a08b7af5054831aa763b8086ddf136
firvain <etsipis@terracognita.eu>
2023-01-08 21:39:01 +0200

    train also

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # Press the green button in the gutter to run the script.
-from Modules.AnomalyDetection import find_anomalies
+from Modules.AnomalyDetection import find_anomalies, train_models
 from Modules.Database import save_df_to_database
 from Modules.scheduler import my_schedule
 
@@ -8,12 +8,13 @@ sensors = ['Teros_12', 'Triscan', 'Scan_chlori', 'Aquatroll', 'Proteus_infinite'
 
 def find_ano():
     for sensor in sensors:
-        # train_models(sensor)
+        train_models(sensor)
         df = find_anomalies(sensor)
-        # print(df)
+        print(df.shape)
         #
         save_df_to_database(df=df, table_name=sensor + "_Anomalies")
 
 
 if __name__ == '__main__':
+    # find_ano()
     my_schedule(find_ano)
