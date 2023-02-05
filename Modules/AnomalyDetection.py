99201fba398e2bc6e51c6dc5fbbaf1a50011a5e7
firvain <etsipis@terracognita.eu>
2023-02-05 12:29:51 +0200

    test path

--- Modules/AnomalyDetection.py
+++ Modules/AnomalyDetection.py
@@ -87,7 +87,8 @@ def train_univariate(sensor, column, series):
 
 def find_anomalies_univariate(sensor, column, test):
     clf = load('./Models/' + sensor + '_' + column + '_' + 'SUOD' + '.joblib')
-
+    print('./Models/' + sensor + '_' + column + '_' + 'SUOD' + '.joblib')
+    quit()
     y_test_pred = clf.predict(test.values.reshape(-1, 1))  # outlier labels (0 or 1)
     quit()
     y_test_scores = clf.decision_function(test.values.reshape(-1, 1))  # outlier scores
